a931ff7bdcf4abc01a7a5e87dd5896eafbfd243b
David Nguyen <hpcsc@users.noreply.github.com>
2023-11-10 23:02:28 +1100

    Move pre-push hook logic to a global library script

--- go-cli/hooks/post_gen_project.sh
+++ go-cli/hooks/post_gen_project.sh
@@ -3,6 +3,3 @@
 mkdir -p ./scripts/libs
 curl https://raw.githubusercontent.com/hpcsc/repo-templates/main/global/libs/git-hook-prepush.sh > ./scripts/libs/git-hook-prepush.sh
 chmod +x ./scripts/libs/git-hook-prepush.sh
-
-go get github.com/rs/zerolog
-go mod tidy
