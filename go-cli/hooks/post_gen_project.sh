dc5c60a6b4e8d45b549d4e9af36a044c81fe2361
David Nguyen <hpcsc@users.noreply.github.com>
2024-03-10 16:26:14 +1100

    Convert go-cli template to use boilerplate

--- go-cli/hooks/post_gen_project.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p ./scripts/libs
-curl https://raw.githubusercontent.com/hpcsc/repo-templates/main/global/libs/git-hook-prepush.sh > ./scripts/libs/git-hook-prepush.sh
-chmod +x ./scripts/libs/git-hook-prepush.sh
